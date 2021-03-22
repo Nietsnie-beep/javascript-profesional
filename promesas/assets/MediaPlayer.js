114bbe9ff365258950edf0c7e0f97744efd626b1
rabbt <albertnio@live.com.mx>
2021-03-21 18:36:09 -0600

    first commit

---
+++ promesas/assets/MediaPlayer.js
@@ -0,0 +1,45 @@
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+  const player = {
+    play: () => this.play(),
+    pause: () => this.pause(),
+    media:this.media,
+    getmuted(){
+      return this.media,muted
+    }
+  }
+};
+
+player.muted()
+
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
+export default MediaPlayer;
